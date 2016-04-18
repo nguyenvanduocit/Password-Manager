5970efc933d74e40d908edce6ba8fc02bcc7714c
Duoc Nguyen <nguyenvanduocit@gmail.com>
2016-04-19 04:15:37 +0700

    make redirect from evn

--- config/services.php
+++ config/services.php
@@ -38,6 +38,6 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://passwordmanager.dev/auth/google/callback',
+        'redirect' => env('GOOGLE_CLIENT_REDIRECT'),
     ],
 ];
